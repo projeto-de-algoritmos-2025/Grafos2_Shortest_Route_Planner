1cd4c98f694f9396bf18b61578369a4cfe32fac2
claudiohsc <claudio885henrique@gmail.com>
2025-05-10 19:23:52 -0300

    fix error in graph duplicates >> >> Co-authored-by: EliasOliver21 <eliasoliveiradapaz@hotmail.com>

--- shortest_route_planner/src/GraphView.tsx
+++ shortest_route_planner/src/GraphView.tsx
@@ -1,22 +1,29 @@
 import { useEffect, useRef } from "react";
 import Sigma from "sigma";
-import { createSampleGraph } from "./graph"; // ajuste o caminho se necessário
+import { createSampleGraph } from "./graph";
+import Graph from "graphology";
 
 export function GraphView() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const sigmaInstance = useRef<Sigma | null>(null);
 
   useEffect(() => {
-    const graph = createSampleGraph();
+    const graph: Graph = createSampleGraph();
 
     if (containerRef.current) {
-      new Sigma(graph, containerRef.current);
+      sigmaInstance.current = new Sigma(graph, containerRef.current);
     }
+
+    
+    return () => {
+      sigmaInstance.current?.kill();
+    };
   }, []);
 
   return (
     <div>
       <h1>Truck Route Optimizer</h1>
-      <div ref={containerRef} style={{ height: "500px" }} />
+      <div ref={containerRef} style={{ height: "600px" }} />
     </div>
   );
 }
